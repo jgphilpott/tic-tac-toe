259537ffbed88d5721fb2a24768caf8999a4a323
Jacob Philpott <jacob.gary.philpott@gmail.com>
2019-12-03 16:11:14 -0500

    adding sass

--- app/root.py
+++ app/root.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import sass
 
-app = Flask("Tic-Tac-Toe", template_folder="app/pages")
+app = Flask("Tic-Tac-Toe", template_folder="app", static_folder="app")
 
 @app.route("/")
 def root():
-    return render_template("welcome.html")
+    return render_template("pages/welcome.html")
+
+sass.compile(dirname=("app/styles", "app/styles"))
 
 if __name__ == "__main__":
     app.jinja_env.auto_reload = True
